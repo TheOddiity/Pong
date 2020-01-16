3e42718b9d6db323b3a1adbd39f0cf41f4adadbf
TheOddity <theoddity@protonmail.com>
2020-01-16 05:23:21 +0100

    Credits update

--- include/Credits.h
+++ include/Credits.h
@@ -16,11 +16,16 @@ private:
 	sf::Texture m_logoTexture;
 	sf::Sprite m_logoSprite;
 	
+	sf::Texture m_SFMLTexture;
+	sf::Sprite m_SFMLSprite;
+	
 	sf::Font m_font;
 	mutable sf::Text m_text;
+	float lineSpacing;
+	sf::Text m_thanksText;
 	
-	std::string m_left[5];
-	std::string m_right[5];
+	std::array<std::string, 5> m_left;
+	std::array<std::string, 5> m_right;
 	
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	
